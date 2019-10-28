55c30f1900d6488111d7e9755cb3bdffe9dfdf63
s-cansin <57084553+s-cansin@users.noreply.github.com>
2019-10-28 02:48:48 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,54 @@
+
+
+
+# PinTweet Share Portal
+* A web based platform for sharing twitter statuses as user based created for IDEKA.
+
+## Description
+
+* PinTweet is a simple web based product which lets users to view last tweets from Donald Trump, share with pinning and write own comments and opinions with a slogan.
+
+* PinTweet has a light and user friendly interface to access information fastly from any device.
+
+* PinTweet may contain some security bugs and lacks of functions due to limited time of development.
+
+## Documentation
+* We unfortunately do not have a complete documentation for this product.
+
+## Features
+
+* Standard Features
+  * Mobile compatible interface
+  * Membership system (Register/Login/Delete Pin)
+  * Latest Tweets Pinned by Users (Accessible by guest in homepage)
+  * Live Feed (Last 40 Tweets from Donald Trump's official twitter profile)
+  * Pin This Tweet
+  * Show pinned tweet with author's comments
+* Disadvantages
+  * Code based security leaks (SQL Injection)
+  * Systematic security leak (Passport middleware were not used for authentication)
+  * Non-Relational MongoDB database is hosted in Evennode cloud rather than AWS due to pricing policies
+  * Node.js code is not structured and refactored properly according to OOP and MVC, due to limited development time.
+
+## Installation on Windows, MacOS, Linux
+* Copy shared files from GitHub as zip package into your environment
+* Unzip the documents if necessary, open app.js file with a editor and change the connection string in line 35 with your own to use your particular mongo database. Don't forget to save the changes.
+* Register an account over AWS Platform and fill up credit card information, be aware of over usage costs.
+
+### Cloud Installation
+* Zip the files with a utility, .rar extension is not accepted by AWS. You also need to ensure that App.js and package.json files are in root of compressed .zip file.
+* Create a Node.js web server physically located onto Linux machine.
+* Upload and deploy the changed .zip file over AWS Elastic Beanstalk Dashboard. You will not need to type unix code over console.
+
+### Test Installation
+* If you need to test web application, you can use AWS Cloud9 Platform (https://console.aws.amazon.com/cloud9/)
+* Drag and drop the files without compressing onto Cloud9 Environment tab.
+* Start a new terminal and run "node app.js" command.
+* Click "Preview" to review your website.
+
+## Have a question? Need assistance?
+* Let me know and i’ll get back to you soon. Write me to samedcansin@gmail.com
+
+## About the author
+* Samed Cansin Eskioglu is a full-stack web developer from Montreal, creating responsive enterprise systems and informatics architectures for the Web. 
+* Don't forget to review earlier canadian projects over portfolio website (http://www.samedcansin.com)
---
+++ app.js
@@ -0,0 +1,360 @@
+var express = require("express");
+
+var bodyParser = require("body-parser");
+var expressSanitizer = require("express-sanitizer")
+var methodOverride = require("method-override");
+var session = require('express-session');
+
+var mongoose = require("mongoose");
+
+var passport = require("passport");
+
+var LocalStrategy = require("passport-local");
+
+var passportLocalMongoose = require("passport-local-mongoose");
+
+var app = express();
+
+var http = require('http');
+
+var Twitter = require('twitter');
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+var jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://f0a03ddc81807c2bfa85212aa379dc16:karanfil@10a.mongo.evennode.com:27017,10b.mongo.evennode.com:27017/f0a03ddc81807c2bfa85212aa379dc16?replicaSet=us-10", {useNewUrlParser:true});
+
+var tweetSchema = new mongoose.Schema({title:String, image:String, comment:String, datetime:Date, cookingTime:Number, tID:String, author:String})
+
+var pinnedtweets = mongoose.model("tweet", tweetSchema);
+
+var userSchema = new mongoose.Schema({username:String, password:String})
+
+var user = mongoose.model("user", userSchema);
+
+var client = new Twitter({
+  consumer_key: 'QBrLJXGaHizZIQlG7Nhchcs52',
+  consumer_secret: '1SPoEwaJFWmq7HHlzFr5d8F6mOyyEn08JrAQ692Fl024YYcXBk',
+  access_token_key: '1187730839670607874-Q6cOkorS75WUvD8zLFdtmd0bi4h5v0',
+  access_token_secret: 'KiW3hpOdw8VzEfO1e5x9p1QKMdJA45BXNVtkCyqP1HWyl'
+});
+
+
+var directory = "/";
+
+
+app.get(directory + "register", function(req,res){
+       if (!req.session.loggedin) {
+res.render("register", { username:req.session.username, host: "http://"+req.headers.host, directory:directory });}
+else
+{
+          res.redirect("./")
+}
+})
+
+
+app.get(directory +"login", function(req,res){
+      if (!req.session.loggedin) {
+res.render("login", { username:req.session.username, host: "http://"+req.headers.host, directory:directory });
+}
+else
+{
+          res.redirect("./")
+}
+})
+
+
+app.get(directory +"logout", function(req,res){
+    if (req.session.loggedin) {
+                req.session.loggedin = false;
+				req.session.username = null;
+				res.redirect("./");
+    }
+})
+
+
+
+
+
+
+
+
+app.get(directory, function(req,res){
+
+pinnedtweets.find({},function(err,tweets)
+{
+    if (err)
+    {
+                       res.render('message', {username:req.session.username, host: "http://"+req.headers.host, directory:directory,message:"Error occured and could not retrieve data! <a href='javascript:history.back()'>Go Back</a>"})
+    }
+    else
+    {
+        
+                    res.render("index",{tweets: tweets, username: req.session.username,  host: "http://"+req.headers.host, directory:directory})
+        
+    }
+    
+    
+}).sort({'_id': -1})
+
+    
+
+})
+
+
+
+
+
+app.get(directory+"contact/", function(req,res){
+
+ res.render("contact",{username: req.session.username, host: "http://"+req.headers.host, directory:directory})
+
+})
+
+
+
+app.get(directory +"show/:id", function(req,res){
+
+pinnedtweets.findById(req.params.id, function (err, foundRec) {
+    if (err) { 
+                              res.render('message', {username:req.session.username, host: "http://"+req.headers.host, directory:directory, message:err})
+ 
+    }
+    else { 
+	
+	
+	
+	
+	
+	
+	
+	
+				
+					 
+			 const params = {
+				 id: foundRec.tID
+			  } 
+
+
+			 client.get('statuses/show', params, function(error, tweets, response){
+
+		
+			  res.render('show', { tweet: foundRec, feed:tweets , username: req.session.username, host: "http://"+req.headers.host, directory:directory});
+			});
+    
+	
+	
+
+	
+	
+      
+    }
+
+})
+
+})
+
+
+
+app.get(directory +"delete/:id", function(req,res){
+  
+  
+  
+  pinnedtweets.remove({ _id: req.params.id }, function(err) {
+    if (!err) {
+            res.redirect(directory);
+    }
+    else {
+            res.redirect(directory);
+    }
+});
+   
+    
+
+
+})
+
+
+app.post(directory +"login",urlencodedParser, function(req,res){
+const username = req.body.username
+const password  = req.body.password
+var newUser = user({
+        username:  username,
+        password: password
+})
+    
+    
+    
+
+user.findOne({'username': req.body.username, 'password': req.body.password}, function(err, record)
+{
+    
+    if (record === null)
+    {
+                      res.render('message', {username:req.session.username, host: "http://"+req.headers.host, directory:directory, message:"Your user/password pair is wrong! <a href='javascript:history.back()'>Go Back</a>"})
+    }
+    else
+    {
+        
+    if (err)
+    {
+    }
+    else
+    {
+            if (!req.session.loggedin) {
+                req.session.loggedin = true;
+				req.session.username = username;
+                res.redirect("./")
+            }   
+    }
+    }
+         
+    
+    
+    
+})
+
+
+
+
+
+})
+
+
+app.post(directory +"register", urlencodedParser, function(req,res){
+
+
+const username = req.body.username
+const password  = req.body.password
+   var newUser = user({
+        username:  username,
+        password: password
+    })
+    
+    
+
+ newUser.save(function(err,record){
+	           
+
+        if (err)
+        {
+            res.render('message', {username:req.session.username, host: "http://"+req.headers.host, directory:directory,message:"Error happened in your user registration! <a href='javascript:history.back()'>Go Back</a>"})
+        }
+        else
+        {
+                        res.render('message', {username:req.session.username, host: "http://"+req.headers.host, directory:directory, message:"Thank you "+username+"!<br>You are registered with id:" + record._id + "<br><a href='" + directory + "'>Home</a>"})
+
+        }
+        
+    } );
+    
+    
+
+ 
+})
+ 
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+app.get(directory + "livefeed", function(req,res){
+ 
+ const params = {
+     from: 'realDonaldTrump', count: 40
+  } 
+
+
+ client.get('search/tweets', params, function(error, tweets, response){
+
+   var tw = tweets.statuses;
+   res.render("live", { username:req.session.username,tweets:tw, host: "http://"+req.headers.host, directory:directory})
+
+});
+                 
+
+})
+
+ 
+ 
+ 
+ 
+ 
+app.get(directory + "pinnew/:id", function(req,res){
+	
+    if (req.session.loggedin) {
+                    res.render("pinnew", { username:req.session.username, tID: req.params.id, host: "http://"+req.headers.host, directory:directory})
+    }
+    else
+    {
+           res.render('message', {username:req.session.username, host: "http://"+req.headers.host, directory:directory, message:"You have to be logged-in to pin new tweet! <a href='" + directory + "login'>Login Now!</a>"})
+
+    }
+})
+
+app.post(directory +"pinnew/:id", urlencodedParser, function(req,res){
+
+
+   var newTweet = pinnedtweets({
+        title:req.body.title,
+        comment:req.body.comment,
+		datetime:new Date().toString().slice(0, 24),
+        tID:req.body.tID,
+		author:req.session.username
+    })
+    
+    newTweet.save(function(err,record){
+        
+  
+        if (err)
+        {
+  
+                   res.render('message', {username:req.session.username, host: "http://"+req.headers.host, directory:directory, message:"error happened in pinning new tweet into database."})
+        }
+        else
+        {
+           // console.log("new record added into database with id:" + record._id);
+           res.render('message', {username:req.session.username, host: "http://"+req.headers.host, directory:directory, message:"new tweet was pinned into database with id:" + record._id + "<br><a href='" + directory + "'>Home</a>"})
+        }
+        
+    })
+    
+})
+
+
+
+
+		   
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, "0.0.0.0", function () {
+    console.log("PinTweet Server has started");
+});
